76de2130d2aa98959b3990687cd4cfabb65cee3c
Vladislav Mikhailov <vladi.mikh.vm@gmail.com>
2022-09-16 09:07:51 +0300

    fix texts

--- src/components/widget/widget.js
+++ src/components/widget/widget.js
@@ -229,7 +229,7 @@ class Widget {
                                 </svg>
                             </button>
                             <label class="attach_screenshot" for="attach_screenshot">
-                                <input id="attach_screenshot" type="file">
+                                <input id="attach_screenshot" type="file" accept="image/png, image/jpeg">
                                 ${attachScreenTitle}
                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M9.93599 15.6737L17.5326 8.3151C17.949 7.89598 18.1829 7.32754 18.1829 6.73483C18.1829 6.14212 17.949 5.57368 17.5326 5.15457C17.1162 4.73545 16.5514 4.5 15.9625 4.5C15.3736 4.5 14.8089 4.73545 14.3924 5.15457L6.61347 12.9681C5.90053 13.6857 5.5 14.6589 5.5 15.6737C5.5 16.6885 5.90053 17.6617 6.61347 18.3793C7.32641 19.0969 8.29336 19.5 9.30162 19.5C10.3099 19.5 11.2768 19.0969 11.9898 18.3793L18.5 11.8428" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -248,7 +248,7 @@ class Widget {
 
                     <h2 class="widget_back__headline">Bug report system based</br>on Google Sheets</h2>
                     <ul class="menu" id="menu">
-                        <li class="menu__item"><a href="https://github.com/vmikh/bugbox" target="_blank">full manual</a></li>
+                        <li class="menu__item"><a href="https://github.com/vmikh/bugbox" target="_blank">github manual</a></li>
                         <li class="menu__item"><a href="mailto:info@bugbox.io">info@bugbox.io</a></li>
                     </ul>
                     <a href="https://yoomoney.ru/to/41001838339242" class="widget_back__date_button" target="_blank">Donate</a>
